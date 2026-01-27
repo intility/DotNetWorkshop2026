cdf64159f14a99c728ad02b16d597272ab11e541
Iver Wellerop <iver.wellerop@intility.no>
2026-01-27 15:46:26 +0100

    feat: add Person model

---
+++ Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace DotnetWorkshop2026.Models;
+
+public class Person
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public int? Height { get; set; }
+
+    public Person? Mother { get; set; }
+    public Person? Father { get; set; }
+
+    public string GetFullName() => $"{FirstName} {LastName}";
+
+    public string GetMotherFullName() => Mother?.GetFullName() ?? "Unknown";
+
+    public string GetFatherFullName() => Father?.GetFullName() ?? "Unknown";
+}
